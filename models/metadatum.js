c1637f5c93e8fd55045732037c1c1696077c3623
Tyler <tyler.william.palmer@gmail.com>
2018-12-20 14:31:39 -0700

    data model

---
+++ models/metadatum.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const dataSchema = new Schema({
+    FileName: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    Directory: {
+        type: String,
+        lowercase: true,
+    },
+    FileSize:{
+        type: String,
+    },
+    FileType:{
+        type: String,
+    },
+    ImageWidth:{
+        type: Number
+    },
+    ImageHeight:{
+        type: Number
+    },
+    Make:{
+        type: String
+    },
+    Model:{
+        type: String
+    },
+    ISO: {
+        type: Number
+    },
+    ShutterSpeedValue: {
+        type: String
+    },
+    ApertureValue: {
+        type: Number
+    },
+    FocalLength:{
+        type: String
+    },
+    Quality: {
+        type: String
+    },
+    Lens: {
+        type: String
+    },
+    LensID:{
+        type: String
+    },
+    Megapixels: {
+        type: Number
+    },
+    FOV: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model('Metadatum', dataSchema)
--- server.js
+++ server.js
@@ -13,14 +13,8 @@ app.use(morgan)('dev')
 // Routes
 
 app.use('/photos', require('./routes/photos'))
-app.use('/customer', require('./routes/customer'))
-
-// Global Error Handler
-
-app.use((err, req, res, next) => {
-    console.log(err)
-    return res.send({errMsg: err.message},"It's broken. Fix it.")
-})
+app.use('/customer', require('./routes/customers'))
+app.use('/metadata', require('./routes/metadata'))
 
 // Connect to the DB
 
@@ -28,6 +22,14 @@ mongoose.connect('mongodb://localhost:27017/photoweb', {useNewUrlParser: true}, 
     console.log("We're connected, captain!")
 })
 
+// Global Error Handler
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+// Listening
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
