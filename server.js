c6ba3b689b9ae9f72939cabbb6e1a242826e8aa8
tyler <tyler.william.palmer@gmail.com>
2019-04-15 03:02:10 -0600

    deploy

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 7001
+const PORT = process.env.PORT || 9000
 require('dotenv').config()
+const path = require("path")
+
 
 // Middleware
 app.use(express.json({limit: '200mb'}))
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 // Routes
 
@@ -16,7 +19,7 @@ app.use('/photos', require('./routes/photos'))
 app.use('/metadata', require('./routes/metadata'))
 // Connect to the DB
 
-mongoose.connect('mongodb://localhost:27017/photoweb', {useNewUrlParser: true, useCreateIndex: true}, () => {
+mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost:27017/photoweb', {useNewUrlParser: true, useCreateIndex: true}, () => {
     console.log("We're connected, captain!")
 })
 
@@ -26,6 +29,10 @@ app.use((err, req, res, next) => {
     console.log(err)
     return res.send({errMsg: err.message})
 })
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Listening
 app.listen(PORT, () => {
