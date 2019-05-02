67e76c683490de0825307f22e4fa34d59120ab03
tyler <tyler.william.palmer@gmail.com>
2019-05-01 21:26:10 -0600

    testing out graphql

--- server.js
+++ server.js
@@ -5,12 +5,45 @@ const mongoose = require('mongoose')
 const PORT = process.env.PORT || 9000
 require('dotenv').config()
 const path = require("path")
+const expressGraphQL = require('express-graphql')
+const { buildSchema } = require('graphql')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 
 // Middleware
 app.use(express.json({limit: '200mb'}))
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(
+    "/graphql",
+    expressGraphQL({
+        schema: buildSchema(`
+            type RootQuery {
+                photos: [String!]!
+            }
+
+            type RootMutation {
+                createPhoto(name: String!): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            photos: () => {
+                return ['thing1', 'thing2', 'thing3', 'thing4']
+            },
+            createPhoto: (args) => {
+                const photoName = args.name
+                return photoName
+            }
+        },
+        graphiql: true
+    })
+)
 
 // Routes
 
@@ -21,7 +54,7 @@ app.use('/metadata', require('./routes/metadata'))
 
 mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost:27017/photoweb', {useNewUrlParser: true, useCreateIndex: true}, () => {
     console.log("We're connected, captain!")
-})
+}).catch(err => console.log(err))
 
 // Global Error Handler
 
