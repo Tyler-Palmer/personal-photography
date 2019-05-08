7dd3345a3e763a2115ae50e267e43f765823fb46
tyler <tyler.william.palmer@gmail.com>
2019-05-08 16:01:20 -0600

    data now relational and linked events to customers

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ const eventsRouter = express.Router();
 const expressGraphQL = require("express-graphql");
 const { buildSchema } = require("graphql");
 const Event = require("../models/event");
+const Customer = require("../models/customer");
 const bcrypt = require("bcryptjs");
 
 eventsRouter.use(
@@ -67,13 +68,25 @@ eventsRouter.use(
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
-                });
+                    date: new Date(args.eventInput.date),
+                    creator: "5cd33c7f04bcd026b752e034"
+                })
+                let createdEvent;
                 return event
                     .save()
                     .then(result => {
-                        console.log(result);
-                        return { ...result._doc };
+                        createdEvent = { ...result._doc }
+                        return Customer.findById('5cd33c7f04bcd026b752e034')
+                    })
+                    .then(customer => {
+                        if (!customer) {
+                            throw new Error("Customer not found");
+                        }
+                        customer.createdEvents.push(event)
+                        return customer.save()
+                    })
+                    .then(result => {
+                        return createdEvent
                     })
                     .catch(err => {
                         console.log(err);
@@ -82,16 +95,22 @@ eventsRouter.use(
             },
 
             createCustomer: args => {
-                return bcrypt
-                    .hash(args.customerInput.password, 12)
+                return Customer.findOne({ email: args.customerInput.email })
+                    .then(customer => {
+                        if (customer) {
+                            throw new Error("User exists already");
+                        }
+                        return bcrypt.hash(args.customerInput.password, 12);
+                    })
                     .then(hashedPassword => {
                         const customer = new Customer({
                             email: args.customerInput.email,
                             password: hashedPassword
                         });
-                        return customer.save()
-                    }).then(result => {
-                        return { ...result._doc }
+                        return customer.save();
+                    })
+                    .then(result => {
+                        return { ...result._doc, password: null };
                     })
                     .catch(err => {
                         console.log(err);
